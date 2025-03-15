1b58150da78e8bbab87960a2a2fea43075c52d0b
Jakob Pistohl <gh@jp-bits.de>
2025-03-16 00:46:25 +0100

    Fix youtube_parser artists

--- youtube_parser.py
+++ youtube_parser.py
@@ -10,7 +10,7 @@ def add_playlist(text):
 def add_vid(text):
     title = __fix_yt_title(text.title.decode_contents().replace(" - YouTube", ""))
     url = re.sub(".*/watch\\?v=", "", text.find("link", rel="canonical")["href"]).split("&", 1)[0]
-    artists = __fix_yt_artist(text.find("meta", property="og:video:tag")["content"])
+    artists = __fix_yt_artist(text.find("link", itemprop="name")["content"])
     year = text.find("meta", itemprop="datePublished")["content"][0:4]
     return [
         music_struct.song(
